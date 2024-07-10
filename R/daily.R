706158aadf896925ba6b7c5f2883669682834feb
Jacob Greer <jacobmgreer@users.noreply.github.com>
2024-07-09 19:39:46 -0700

    Update daily.R

--- R/daily.R
+++ R/daily.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(jsonlite)
 
+Sys.setenv(TZ="America/Los_Angeles")
+
 options(
   readr.num_columns = 0, 
   readr.show_col_types = FALSE,
@@ -15,7 +17,9 @@ flat_art <- function(data) {
 
 dir.create("nightlies", showWarnings = FALSE)
 dir.create("nightlies/onview", showWarnings = FALSE)
+dir.create(paste("nightly/onview/", format(Sys.time(), "%Y"), showWarnings = FALSE))
 dir.create("nightlies/gh-objects", showWarnings = FALSE)
+dir.create(paste("nightly/gh-objects/", format(Sys.time(), "%Y"), showWarnings = FALSE))
 
 ## ON VIEW
 download.file("https://www.nga.gov/collection-search-result/jcr:content/parmain/facetcomponent/parList/collectionsearchresu.pageSize__3000.pageNumber__1.json?onview=On_View&_=1554781398824",
@@ -29,7 +33,7 @@ onview <-
 
 onview <- jsonlite::fromJSON(onview)["results"][[1]]
 
-saveRDS(onview, file = paste0("RData/onview/", format(Sys.time(), "%Y.%m.%d"), ".rds"))
+saveRDS(onview, file = paste0("RData/onview/", format(Sys.time(), "%Y"), "/", format(Sys.time(), "%Y.%m.%d"), ".rds"))
 
 ## GH-NGA-Objects
 download.file("https://raw.githubusercontent.com/NationalGalleryOfArt/opendata/main/data/objects.csv",
@@ -49,6 +53,6 @@ gh_objects <-
     lastdetectedmodification, wikidataid
   )
 
-saveRDS(gh_objects, file = paste0("RData/gh-objects/", format(Sys.time(), "%Y.%m.%d"), ".rds"))
+saveRDS(gh_objects, file = paste0("RData/gh-objects/", format(Sys.time(), "%Y"), "/", format(Sys.time(), "%Y.%m.%d"), ".rds"))
 
 unlink("nightlies", recursive = TRUE, force = TRUE)
